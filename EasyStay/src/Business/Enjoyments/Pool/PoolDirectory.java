9eebdd97ebb9a16661bc261165277ffe9a78e3b4
Rahul Ramesh Patil <patil.rahu@northeastern.edu>
2024-12-02 00:20:13 -0500

    Adding pool directory class

---
+++ EasyStay/src/Business/Enjoyments/Pool/PoolDirectory.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Enjoyments.Pool;
+
+import Business.VehicleService.Booking.Vehicle;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author RAHUL
+ */
+public class PoolDirectory {
+    ArrayList<Pool> poolDirectory;
+    
+    public PoolDirectory(){
+        this.poolDirectory = new ArrayList<>();
+    }
+
+    public ArrayList<Pool> getPoolDirectory() {
+        return poolDirectory;
+    }
+
+    public void setPoolDirectory(ArrayList<Pool> poolDirectory) {
+        this.poolDirectory = poolDirectory;
+    }
+    
+    public Pool getPools(int index){
+        Pool newPool = new Pool();
+        newPool = poolDirectory.get(index);
+        return newPool;
+    }
+    
+    public Pool addPoolDetails(){
+        Pool newPool = new Pool();
+        poolDirectory.add(newPool);
+        
+        return newPool;
+    }
+    
+    public Pool getPoolByName(String poolName) {
+        
+        Pool selectedPool = new Pool();
+        for(Pool pool : poolDirectory) {
+            if(pool.getPoolName().equals(poolName)) {
+                selectedPool = pool;
+                break;
+            }
+        }
+        return selectedPool;
+    }
+    
+    public void deletePool(Pool pool){
+        poolDirectory.remove(pool);
+    }
+    
+}
